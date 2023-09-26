f7a762cd518ce4a566087efe18c1e71ab345e584
andre_alves <andre_alves@ACER_ASPIRE>
2023-09-26 13:41:58 -0300

    update projetojava1

--- projetojava1/src/modulo7/tiposDados.java
+++ projetojava1/src/modulo7/tiposDados.java
@@ -1,16 +1,9 @@
 package modulo7;
 
-public class tiposDeDados {
+public class tiposDados {
 
 	public static void main(String[] args) {
-		// Tipo          // Tamanho
-		byte tipoByte;   //  8 bits
-		short tipoShort; // 16 bits
-		int tipoInt;     // 32 bits
-		long tipoLong;
-		double tipoDouble;
-		boolean tipoBoolean;
-		String tipoString;
+		
 		
 	}
 
