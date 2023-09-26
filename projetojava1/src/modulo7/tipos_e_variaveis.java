39f1c9cc87ac1214d825d400b52006409f7828f3
andre_alves <andre_alves@ACER_ASPIRE>
2023-09-26 13:16:42 -0300

    d

---
+++ projetojava1/src/modulo7/tipos_e_variaveis.java
@@ -0,0 +1,17 @@
+package modulo7;
+
+public class tipos_e_variaveis {
+
+	public static void main(String[] args) {
+		// Tipo          // Tamanho
+		byte tipoByte;   //  8 bits
+		short tipoShort; // 16 bits
+		int tipoInt;     // 32 bits
+		long tipoLong;
+		double tipoDouble;
+		boolean tipoBoolean;
+		String tipoString;
+		
+	}
+
+}
