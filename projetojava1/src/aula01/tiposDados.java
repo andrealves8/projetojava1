5fa2e8c5a0984caf1b5aadabc2fcae0adc4d36aa
andre_alves <andre_alves@ACER_ASPIRE>
2023-09-28 11:44:29 -0300

    update aula01

---
+++ projetojava1/src/aula01/tiposDados.java
@@ -0,0 +1,19 @@
+package aula01;
+
+public class tiposDados {
+
+	public static void main(String[] args) {
+		
+		int v1 = 10;
+		double v2 = 5.5;
+		char v3 = 'A';
+		boolean v4 = true;
+		
+		System.out.println(v1);
+		System.out.println(v2);
+		System.out.println(v3);
+		System.out.println(v4);
+		
+	}
+
+}
